e5d72446dfcbe3ecad70bd7c68c7eb784209e9c6
Shaikh Tashif <Shailhtashif87@gmail.com>
2026-02-22 15:15:29 +0530

    Add arithmetic operations and list element printing
    Implement basic arithmetic operations and list slicing.

---
+++ problem's solving.py
@@ -0,0 +1,9 @@
+# I'm working with a subtraction, multiplication, additions, dividations 
+a = 10
+b = 20
+sum = a*b, a-b, a+b, a/b
+print(sum)
+
+# solving a problem for a list print 6 elements 
+list = ["apple", "Banana", 10, 20, 30, 40, 50, 60, 70, 80]
+print(list[:6])
